e2e7e6224779209bb74cba6cc8353296190a9194
TiannaDeSpain <u663140950@byui.edu>
2020-11-21 21:02:10 -0700

    tried speeding up page

--- lesson10/js/script.js
+++ lesson10/js/script.js
@@ -18,3 +18,36 @@ var today = new Date();
 if(today.getDay() == 5){
     document.getElementById("announcment").style.display = "block";
     }
+
+
+    imagesToLoad = document.querySelectorAll("img[data-src]");
+    imgOptions = {
+        threshold: 1,
+        rootMargin: "0px 0px 100px 0px"
+    };
+    
+     loadImages = (image) => {
+        image.setAttribute('src', image.getAttribute('data-src'));
+        image.onload = () => {
+            image.removeAttribute('data-src');};
+    };
+        if('IntersectionObserver' in window) {
+            const imgObserver = new IntersectionObserver((items, observer) => {
+                items.forEach((item) => {
+                    if(item.isIntersecting) {
+                        loadImages(item.target);
+                        imgObserver.unobserve(item.target);
+                    }
+                });
+            }, imgOptions );
+    
+        imagesToLoad.forEach((img) => {
+          imgObserver.observe(img);
+        });
+        } 
+        else {
+        imagesToLoad.forEach((img) => {
+          loadImages(img);
+        });
+      }
+    
