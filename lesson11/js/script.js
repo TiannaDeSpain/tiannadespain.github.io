a0e66c85052c9d884efa23af0b19081e20384d63
TiannaDeSpain <u663140950@byui.edu>
2020-11-30 08:52:20 -0700

    scripts for banner Announcements

--- lesson11/js/script.js
+++ lesson11/js/script.js
@@ -18,36 +18,41 @@ var today = new Date();
 if(today.getDay() == 5){
     document.getElementById("announcment").style.display = "block";
     }
+else if(today.getDay() == 4){
+    document.getElementById("fhAnnouncment").style.display = "block";
+    }
+else if(today.getDay() == 0){
+    document.getElementById("ssAnnouncment").style.display = "block";
+    }
 
 
-    imagesToLoad = document.querySelectorAll("img[data-src]");
-    imgOptions = {
-        threshold: 1,
-        rootMargin: "0px 0px 100px 0px"
-    };
-    
-     loadImages = (image) => {
-        image.setAttribute('src', image.getAttribute('data-src'));
-        image.onload = () => {
-            image.removeAttribute('data-src');};
-    };
-        if('IntersectionObserver' in window) {
-            const imgObserver = new IntersectionObserver((items, observer) => {
-                items.forEach((item) => {
-                    if(item.isIntersecting) {
-                        loadImages(item.target);
-                        imgObserver.unobserve(item.target);
-                    }
-                });
-            }, imgOptions );
-    
-        imagesToLoad.forEach((img) => {
-          imgObserver.observe(img);
+imagesToLoad = document.querySelectorAll("img[data-src]");
+imgOptions = {
+    threshold: 1,
+    rootMargin: "0px 0px 100px 0px"
+};
+
+    loadImages = (image) => {
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {
+        image.removeAttribute('data-src');};
+};
+if('IntersectionObserver' in window) {
+    const imgObserver = new IntersectionObserver((items, observer) => {
+        items.forEach((item) => {
+            if(item.isIntersecting) {
+                loadImages(item.target);
+                imgObserver.unobserve(item.target);
+            }
         });
-        } 
-        else {
-        imagesToLoad.forEach((img) => {
-          loadImages(img);
-        });
-      }
-    
+    }, imgOptions );
+
+imagesToLoad.forEach((img) => {
+    imgObserver.observe(img);
+});
+} 
+else {
+imagesToLoad.forEach((img) => {
+    loadImages(img);
+});
+}
