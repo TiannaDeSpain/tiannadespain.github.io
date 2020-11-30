f10c1dca58b7b4e60a37596dd91e4f40e66b37db
TiannaDeSpain <u663140950@byui.edu>
2020-11-30 08:38:47 -0700

    Week 11 intial Push

---
+++ lesson11/js/observe.js
@@ -0,0 +1,31 @@
+
+imagesToLoad = document.querySelectorAll("img[data-src]");
+imgOptions = {
+    threshold: 1,
+    rootMargin: "0px 0px 100px 0px"
+};
+
+ loadImages = (image) => {
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {
+        image.removeAttribute('data-src');};
+};
+    if('IntersectionObserver' in window) {
+        const imgObserver = new IntersectionObserver((items, observer) => {
+            items.forEach((item) => {
+                if(item.isIntersecting) {
+                    loadImages(item.target);
+                    imgObserver.unobserve(item.target);
+                }
+            });
+        }, imgOptions );
+
+    imagesToLoad.forEach((img) => {
+      imgObserver.observe(img);
+    });
+    } 
+    else {
+    imagesToLoad.forEach((img) => {
+      loadImages(img);
+    });
+  }
