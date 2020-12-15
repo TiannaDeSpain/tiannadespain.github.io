ec34ee6b47144f6c961b4ee0b037580137b10948
TiannaDeSpain <u663140950@byui.edu>
2020-12-15 15:57:17 -0700

    moved json to data folder

--- finalproject/js/rentals.js
+++ finalproject/js/rentals.js
@@ -1,6 +1,34 @@
-const requestURL = 'https://github.com/TiannaDeSpain/finalwebsitewdd230/blob/master/js/rentals.json';
 
-fetch(requestURL)
-  .then(function (response) {
-    return response.json();
-  })
+$.getJSON("https://tiannadespain.github.io/finalproject/data/rentals.json", function(json) {
+    console.log(json); 
+  var data = json;
+  data.forEach(tablerow);
+
+  function tablerow(item) {
+    let table = document.getElementById("rentalsTable")
+    let tr = document.createElement("tr")
+    let RentalType = document.createElement('td');
+    let MaxPersons = document.createElement('td');
+    let ReservationsHalf = document.createElement('td');
+    let ReservationsWhole = document.createElement('td');
+    let WalkHalf = document.createElement('td');
+    let WalkWhole = document.createElement('td');
+
+    RentalType.innerHTML =  item.RentalType;
+    MaxPersons.innerHTML =  item.MaxPersons;
+    ReservationsHalf.innerHTML =  item.ReservationsHalf;
+    ReservationsWhole.innerHTML =  item.ReservationsWhole;
+    WalkHalf.innerHTML =  item.WalkHalf;
+    WalkWhole.innerHTML = item.WalkWhole;
+
+    tr.appendChild(RentalType);
+    tr.appendChild(MaxPersons);
+    tr.appendChild(ReservationsHalf);
+    tr.appendChild(ReservationsWhole);
+    tr.appendChild(WalkHalf);
+    tr.appendChild(WalkWhole);
+
+    table.appendChild(tr)
+    
+  }
+});
